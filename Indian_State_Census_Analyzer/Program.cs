942ab39b24ae8f7c1a524536595f49b8746a0784
Arka <arka.spectacular@gmail.com>
2021-08-26 23:38:29 +0530

    [Arka]Add:Welcome Message

---
+++ Indian_State_Census_Analyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Indian_State_Census_Analyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analyzer Program!");
+        }
+    }
+}
