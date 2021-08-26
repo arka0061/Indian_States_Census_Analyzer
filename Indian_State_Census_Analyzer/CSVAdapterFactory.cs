004c3a2edabd2406064e57709fb987dcb877044d
Arka <arka.spectacular@gmail.com>
2021-08-27 00:24:22 +0530

    [Arka]Add:Load state code information from csv files

---
+++ Indian_State_Census_Analyzer/CSVAdapterFactory.cs
@@ -0,0 +1,23 @@
+﻿using Indian_State_Census_Analyzer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indian_State_Census_Analyzer
+{
+    public class CSVAdapterFactory
+    {
+        internal Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                case (CensusAnalyser.Country.US):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
